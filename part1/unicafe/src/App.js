28568075798c11b71029c52b642f64e182b24ac0
Vojislav Trujkic <voizlav@gmail.com>
2023-01-12 22:34:32 +0100

    Add no feedback given (step 4)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,10 +3,21 @@ import { useState } from 'react'
 
 const Header = ({heading}) => <h1>{heading}</h1>
 
-// Note: It seems that I did step 3 already in step 1 instinctively
-const Stats = ({feedback, data}) => <p>{feedback} {data}</p>
-
 const Button = ({trigger, feedback}) => <button onClick={trigger}>{feedback}</button>
+
+const Stat = ({feedback, data}) => <p>{feedback} {data}</p>
+
+const Stats = ({feedback: {good, neutral, bad}}) => 
+  good + neutral + bad === 0 ? <p>No feedback given</p> : (
+    <>
+      <Stat feedback={'good'} data={good}/>
+      <Stat feedback={'neutral'} data={neutral}/>
+      <Stat feedback={'bad'} data={bad}/>
+      <Stat feedback={'all'} data={good + neutral + bad} />
+      <Stat feedback={'average'} data={(good - bad) / (good + neutral + bad)} />
+      <Stat feedback={'positive'} data={`${(good) / (good + neutral + bad) * 100}%`} />
+    </>
+  )
 
 
 const App = () => {
@@ -15,19 +26,14 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
+    <>
       <Header heading={'Give feedback'} />
       <Button feedback={'good'} trigger={() => setGood(good + 1)} />
       <Button feedback={'neutral'} trigger={() => setNeutral(neutral + 1)} />
       <Button feedback={'bad'} trigger={() => setBad(bad + 1)} />
       <Header heading={'statistics'} />
-      <Stats feedback={'good'} data={good} />
-      <Stats feedback={'neutral'} data={neutral} />
-      <Stats feedback={'bad'} data={bad} />
-      <Stats feedback={'all'} data={good + neutral + bad} />
-      <Stats feedback={'average'} data={(good - bad) / (good + neutral + bad)} />
-      <Stats feedback={'positive'} data={`${(good) / (good + neutral + bad) * 100}%`} />
-    </div>
+      <Stats feedback={{'good': good, 'neutral': neutral, 'bad': bad}} />
+    </>
   )
 }
 
