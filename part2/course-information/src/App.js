3905120e270da891409134560d65bf7d4c231095
Vojislav Trujkic <voizlav@gmail.com>
2023-01-14 02:23:19 +0100

    Update code clean up (step 6)

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -2,22 +2,16 @@ const Header = ({ course }) => <h1>{course}</h1>
 
 const Total = ({ sum }) => <p>Number of exercises {sum}</p>
 
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
 const Content = ({ parts }) => 
-  parts.map(part => <Part key={part.id} part={part}/>)
+  parts.map(part => <Part key={part.id} part={part} />)
 
-  
 const Course = ({course}) => 
   <>
     <Header course={course.name} />
     <Content parts={course.parts} />
   </>
-
 
 const App = () => {
   const course = {
