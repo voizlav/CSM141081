59e7ab9c2e1c1982bf455ec8f2d2ccf14315b7d2
Vojislav Trujkic <voizlav@gmail.com>
2023-01-16 03:52:24 +0100

    Add phone number to object (step 3)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,30 +3,32 @@ import { useState } from 'react'
 
 const Header = ({heading}) => <h2>{heading}</h2>
 
-const Display = ({name}) => <p>{name}</p>
+const Display = ({person}) => <p>{person.name} {person.number}</p>
 
 const Button = ({title}) => <button type='submit'>{title}</button>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (e) => {
+  const addPerson = (e) => {
     e.preventDefault()
-
+    
     !persons.map(person => person.name).includes(newName)
-    ? setPersons([...persons, { name: newName }])
+    ? setPersons([...persons, { name: newName, number: newNumber }])
     : alert(`${newName} already in phonebook`)
 
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <Header heading={'Phonebook'} />
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: 
           <input 
@@ -35,11 +37,18 @@ const App = () => {
           />
         </div>
         <div>
+          number: 
+          <input 
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
           <Button title={'new'} />
         </div>
       </form>
       <Header heading={'Numbers'} />
-      {persons.map(person => <Display key={person.name} name={person.name} />)}
+      {persons.map(person => <Display key={person.name} person={person} />)}
     </div>
   )
 }
