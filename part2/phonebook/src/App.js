37b2defe678ae4b012a72ee5be4717937f094da2
Vojislav Trujkic <voizlav@gmail.com>
2023-01-16 05:12:06 +0100

    Add dummy data (step 4)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,7 +12,10 @@ const Input = ({title, value, trigger}) =>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
