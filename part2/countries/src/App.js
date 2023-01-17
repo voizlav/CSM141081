de04d3b79564b6728712e905f9a0db21953670d6
Vojislav Trujkic <voizlav@gmail.com>
2023-01-17 01:41:22 +0100

    Add useState and useEffect to pull data (step 1)

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,6 +2,10 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {axios.get('https://restcountries.com/v3.1/all').then(res => setCountries(res.data))}, [])
+  console.log(countries)
   return (
     <></>
   )
