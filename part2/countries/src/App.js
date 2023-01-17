84665f8479e9e84f4a426d9e1835cd596fd1de3d
Vojislav Trujkic <voizlav@gmail.com>
2023-01-17 03:59:50 +0100

    Implement filtering data (step 1)

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,13 +1,27 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+
 function App() {
   const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
 
-  useEffect(() => {axios.get('https://restcountries.com/v3.1/all').then(res => setCountries(res.data))}, [])
-  console.log(countries)
+  useEffect(() => {axios.get('https://restcountries.com/v3.1/all')
+  .then(res => setCountries(res.data))}, [])
+
+  const filtered = [...countries.filter(country => 
+    country.name.common.toLocaleLowerCase().includes(filter))]
+
+  console.log(filtered)
+
   return (
-    <></>
+    <>
+      find countries: 
+      <input 
+        value={filter} 
+        onChange={(e) => setFilter(e.target.value.toLocaleLowerCase())} 
+      />
+    </>
   )
 }
 
