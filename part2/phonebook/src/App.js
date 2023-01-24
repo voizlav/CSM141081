6d28bdae13bb75c101a138c5ceb6b9c4010a67e8
Vojislav Trujkic <voizlav@gmail.com>
2023-01-24 08:13:52 +0100

    Refactor App to utilize service module (step 8)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,20 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import service from './service/persons'
+
 
 const Header = ({heading}) => <h2>{heading}</h2>
 
+
 const Display = ({person: {name, number}}) => <p>{name} {number}</p>
+
 
 const Button = ({title}) => <button type='submit'>{title}</button>
 
+
 const Input = ({title, value, trigger}) => 
   <div>{title}: <input value={value} onChange={trigger}/></div>
+
 
 const Form = ({add, name, number, newName, newNumber}) =>
   <div>
@@ -21,13 +27,16 @@ const Form = ({add, name, number, newName, newNumber}) =>
     </form>
   </div>
 
+
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-123456'}]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
-  useEffect(() => { axios.get('http://localhost:3001/persons').then(res => setPersons(res.data))}, [])
+  useEffect(() => {
+    service.getAllPersons().then(personsData => setPersons(personsData))
+  }, [])
 
   const addPerson = (e) => {
     e.preventDefault()
@@ -35,8 +44,8 @@ const App = () => {
     persons.find(person => 
       person.name.toLocaleLowerCase() === newName.toLocaleLowerCase())
       ? alert(`${newName} already in phonebook`)
-      : axios.post('http://localhost:3001/persons', { name: newName, number: newNumber })
-      .then(res => setPersons(persons.concat(res.data)))
+      : service.createPerson({ name: newName, number: newNumber })
+      .then(newPersonData => setPersons(persons.concat(newPersonData)))
 
     setNewName('')
     setNewNumber('')
@@ -67,5 +76,6 @@ const App = () => {
     </div>
   )
 }
+
 
 export default App
