a4ac9928f632138d179c3fb9982e66f7b0a856eb
Vojislav Trujkic <voizlav@gmail.com>
2023-01-31 08:40:23 +0100

    Update BASEURL to relative path

--- part3/phonebook/client/src/service/persons.js
+++ part3/phonebook/client/src/service/persons.js
@@ -1,21 +1,14 @@
-import axios from 'axios'
-const BASEURL = 'http://localhost:3001/persons'
+import axios from "axios";
+const BASEURL = "/api/persons";
 
+const getAllPersons = () => axios.get(BASEURL).then((res) => res.data);
 
-const getAllPersons = () => 
-  axios.get(BASEURL).then(res => res.data)
+const createPerson = (newPerson) =>
+  axios.post(BASEURL, newPerson).then((res) => res.data);
 
+const updatePerson = (id, editPerson) =>
+  axios.put(`${BASEURL}/${id}`, editPerson).then((res) => res.data);
 
-const createPerson = (newPerson) => 
-  axios.post(BASEURL, newPerson).then(res => res.data)
+const deletePerson = (id) => axios.delete(`${BASEURL}/${id}`);
 
-
-const updatePerson = (id, editPerson) => 
-  axios.put(`${BASEURL}/${id}`, editPerson).then(res => res.data)
-
-
-const deletePerson = (id) =>
-  axios.delete(`${BASEURL}/${id}`)
-
-
-export default { getAllPersons, createPerson, updatePerson, deletePerson }
+export default { getAllPersons, createPerson, updatePerson, deletePerson };
