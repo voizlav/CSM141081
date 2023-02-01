b128099a87a9266c5d57fbb48524ae72f8605efc
Vojislav Trujkic <voizlav@gmail.com>
2023-02-01 11:14:30 +0100

    Update connecting to database

--- part3/phonebook-cli/mongo.js
+++ part3/phonebook-cli/mongo.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const crypto = require("crypto");
 
 process.argv.length < 3 || process.argv.length > 5 || process.argv.length === 4
   ? (console.error("Usage: node mongo.js password [name] [number]"),
@@ -8,11 +7,10 @@ process.argv.length < 3 || process.argv.length > 5 || process.argv.length === 4
   : null;
 
 process.argv.length === 5
-  ? process.argv[3].length > 30 ||
-    process.argv[3].length < 3 ||
-    process.argv[4].length > 15 ||
-    process.argv[4].length < 3
-    ? (console.error("Name or/and number length out of scope"), process.exit(1))
+  ? process.argv[3].length > 30 || process.argv[3].length < 3
+    ? (console.error("Name length out of scope"), process.exit(1))
+    : process.argv[4].length > 15 || process.argv[4].length < 3
+    ? (console.error("Number length out of scope"), process.exit(1))
     : null
   : null;
 
@@ -24,24 +22,16 @@ process.argv.length === 5
     : null
   : null;
 
-!process.env.DBUSER ||
-!process.env.CLUSTER ||
-!process.env.REGION ||
-!process.env.DATABASE
-  ? (console.error("Must provide database configuration"), process.exit(1))
+!process.env.URI
+  ? (console.error("A database URI must be provided in ENV"), process.exit(1))
   : null;
 
 mongoose.set("strictQuery", false);
-mongoose.connect(
-  `mongodb+srv://` +
-    `${process.env.DBUSER}` +
-    `:${process.argv[2]}` +
-    `@${process.env.CLUSTER}` +
-    `.${process.env.REGION}` +
-    `.mongodb.net/` +
-    `${process.env.DATABASE}` +
-    `?retryWrites=true&w=majority`
-);
+mongoose
+  .connect(process.env.URI.replace(/password/g, process.argv[2]))
+  .catch((error) =>
+    console.error("Failure in establishing a connection to MongoDB")
+  );
 
 const Peeps = mongoose.model(
   "Peeps",
@@ -60,14 +50,13 @@ process.argv.length < 4
       mongoose.connection.close();
     })
   : new Peeps({
-      id: crypto.randomUUID().split("-")[0],
       name: process.argv[3],
       number: process.argv[4],
     })
       .save()
       .then((result) => {
         console.log(
-          `added ${result.name} number ${result.number} to phonebook`
+          `Added ${result.name} number ${result.number} to phonebook`
         );
         mongoose.connection.close();
       });
