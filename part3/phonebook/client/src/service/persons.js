dda89276019c8c73af2574368a244a19178b32cb
Vojislav Trujkic <voizlav@gmail.com>
2023-02-02 12:52:06 +0100

    Remove update person (step 2)

--- part3/phonebook/client/src/service/persons.js
+++ part3/phonebook/client/src/service/persons.js
@@ -6,9 +6,6 @@ const getAllPersons = () => axios.get(BASEURL).then((res) => res.data);
 const createPerson = (newPerson) =>
   axios.post(BASEURL, newPerson).then((res) => res.data);
 
-const updatePerson = (id, editPerson) =>
-  axios.put(`${BASEURL}/${id}`, editPerson).then((res) => res.data);
-
 const deletePerson = (id) => axios.delete(`${BASEURL}/${id}`);
 
 export default { getAllPersons, createPerson, updatePerson, deletePerson };
