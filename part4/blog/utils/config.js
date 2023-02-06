3a6304c77e446a74f85ba1df4599fab617a77805
Vojislav Trujkic <voizlav@gmail.com>
2023-02-06 11:52:19 +0100

    Update config (step 2)

--- part4/blog/utils/config.js
+++ part4/blog/utils/config.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const { error } = require("./logger");
 
-!process.env.DATABASE_URI ? error("No database configuration") : null;
+!process.env.DATABASE_URI
+  ? (error("No database configuration"), process.exit(1))
+  : null;
 
 const PORT = process.env.PORT || 3001;
 const DATABASE_URI = process.env.DATABASE_URI;
