88b40b4d86189b6f7cdb7947a6e6ba1e993adec8
Vojislav Trujkic <voizlav@gmail.com>
2023-02-08 22:37:05 +0100

    Add favoriteBlog function (step 3)

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {};
+
+  return blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max));
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
