9d61cafa0df1e883338e4aa2185989389a3347f5
Vojislav Trujkic <voizlav@gmail.com>
2023-02-08 22:45:26 +0100

    Add test for favoriteBlog with one blog (step 3)

--- part4/blog/tests/list_helpers.test.js
+++ part4/blog/tests/list_helpers.test.js
@@ -117,4 +117,16 @@ describe("most likes", () => {
     const result = favoriteBlog(listNoBlogs);
     expect(result).toEqual({});
   });
+
+  test("when list has only one blog, equals to that object", () => {
+    const result = favoriteBlog(listWithOneBlog);
+    expect(result).toEqual({
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    });
+  });
 });
